e89a97f090ed149a54553276d334c128896cd0f9
sina <89062281+sinaNJN@users.noreply.github.com>
2022-12-29 15:16:41 +0330

    Create Bot.py

---
+++ Bot.py
@@ -0,0 +1,16 @@
+from pyrogram import Client
+
+bot = CLient(
+    session_name = "xui" ,
+    api_id = 1110329,
+    api_hash = "b90f31f3765a434904767080a25db99a",
+    bot_token = '774065700:AAGl_JH0aXzqjUjUxRDvFodPNGI4ASB1PrE'
+    
+)
+
+@bot.on_message()
+async def test(Client , message):
+    await bot.send_message(message.chat.id , 'hello world')
+
+
+bot.run()
