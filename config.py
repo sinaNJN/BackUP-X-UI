44ecb0c7605cfdf04ef8d32d844ad86b14b91804
sina <89062281+sinaNJN@users.noreply.github.com>
2023-01-09 22:10:47 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+user_api_id = input('set api id: ')
+user_api_hash = input('set api hash : ')
+user_bot_token = input('set bot token : ')
+user_pv_id = input('set your Id(usernumber) : ')
+user_path_file = input('set (x-ui.db) path : ')
+
+print ('all data set')
+print ('now use screen & python3 bot.py')
